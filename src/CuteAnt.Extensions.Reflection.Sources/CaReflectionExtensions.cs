a4e4b3f09917bfed2ee7c5b258c228feba128387
Seabiscuit <cuteant@outlook.com>
2017-01-13 20:59:58 +0800

    Implement GetMethodInfo with Type[] params for .NET Core

--- src/CuteAnt.Extensions.Reflection.Sources/CaReflectionExtensions.cs
+++ src/CuteAnt.Extensions.Reflection.Sources/CaReflectionExtensions.cs
@@ -1493,7 +1493,19 @@ namespace System.Reflection
     public static MethodInfo GetMethodInfo(this Type type, string methodName, Type[] types = null)
     {
 #if (NETFX_CORE || PCL || NETSTANDARD)
-      return type.GetRuntimeMethods().FirstOrDefault(p => p.Name.Equals(methodName));
+      if (types == null)
+        return type.GetRuntimeMethods().FirstOrDefault(p => p.Name.Equals(methodName));
+
+      foreach (var mi in type.GetRuntimeMethods().Where(p => p.Name.Equals(methodName)))
+      {
+        var methodParams = mi.GetParameters().Select(p => p.ParameterType);
+        if (methodParams.SequenceEqual(types))
+        {
+          return mi;
+        }
+      }
+
+      return null;
 #else
       return types == null
           ? type.GetMethod(methodName)
