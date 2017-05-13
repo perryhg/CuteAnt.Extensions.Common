d397b1ba2c0edd54d275421d17643338fdd05d1d
Seabiscuit <cuteant@outlook.com>
2017-05-13 19:26:21 +0800

    CuteAnt.Extensions.ReaderWriterLockSlim.Sources

---
+++ src/CuteAnt.Extensions.ReaderWriterLockSlim.Sources/ReaderWriterLockSlimExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Threading
+{
+  internal static class ReaderWriterLockSlimExtensions
+  {
+    internal static IDisposable CreateToken(this ReaderWriterLockSlim rwlock, bool isReadMode = false)
+    {
+      if (null == rwlock) { throw new ArgumentNullException(nameof(rwlock)); }
+      return new ReaderWriterLockToken(rwlock, isReadMode);
+    }
+  }
+}
---
+++ src/CuteAnt.Extensions.ReaderWriterLockSlim.Sources/ReaderWriterLockToken.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Threading
+{
+  internal struct ReaderWriterLockToken : IDisposable
+  {
+    private ReaderWriterLockSlim _rwlock;
+    private Boolean _isReadMode;
+
+    public ReaderWriterLockToken(ReaderWriterLockSlim rwlock, Boolean isReadMode)
+    {
+      _rwlock = rwlock;
+      _isReadMode = isReadMode;
+      if (_rwlock == null) { return; }
+      if (_isReadMode)
+      {
+        _rwlock.EnterReadLock();
+      }
+      else
+      {
+        _rwlock.EnterWriteLock();
+      }
+    }
+
+    public void Dispose()
+    {
+      var rwlock = _rwlock;
+      _rwlock = null;
+
+      if (rwlock != null)
+      {
+        try
+        {
+          if (_isReadMode)
+          {
+            rwlock.ExitReadLock();
+          }
+          else
+          {
+            rwlock.ExitWriteLock();
+          }
+        }
+        catch { }
+      }
+    }
+  }
+}
